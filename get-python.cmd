282644320d99aac5c26af918eb665377d707686b
Nishant Mohanchandra <nm1345@nyu.edu>
2024-08-30 19:40:43 +0530

    Update get-python.cmd

--- get-python.cmd
+++ get-python.cmd
@@ -1,5 +1,3 @@
-Invoke-WebRequest -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.12.5/python-3.12.5-amd64.exe' -OutFile python-3.12.5-amd64.exe
-.\python-3.12.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-setx /M path "%path%;C:\Program Files\Python312"
-$env:PATH =$env:PATH+";C:\Program Files\Python312"
-python --version
+curl -L https://www.python.org/ftp/python/3.12.5/python-3.12.5-amd64.exe -o python-3.12.5-amd64.exe
+.\python-3.12.5-amd64.exe /quiet InstallAllUsers=1 InstallLauncherAllUsers=1 PrependPath=1 Include_test=0
+del "python-3.12.5-amd64.exe"
