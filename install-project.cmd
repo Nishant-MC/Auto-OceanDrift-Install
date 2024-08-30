efcf6380814a77a981d3809055b01b3093dc596f
Nishant Mohanchandra <nm1345@nyu.edu>
2024-08-30 21:22:45 +0530

    Fixed another typo

--- install-project.cmd
+++ install-project.cmd
@@ -1,7 +1,7 @@
 pip install virtualenv virtualenvwrapper-win
 python -m virtualenv .
 Set-ExecutionPolicy RemoteSigned
-.\scripts\activate
+.\PythonScripts\scripts\activate
 pip install numpy psutil setuptools wheel
 pip install opendrift
 deactivate
